033403f9fe666c29b2ebfd0e5d3bfe98d84e9ef3
Oleksiy Oletsky <oletsky@ukr.net>
2020-09-06 13:36:25 +0300

    Add files via upload

--- asyncrread.js
+++ asyncrread.js
@@ -5,7 +5,11 @@ const rl = read.createInterface({
     output: process.stdout
 });
 
-let s=rl.question("Enter your value");
-console.log(s);
-//Undefined; the program doesn't terminate
-//How to fix this
+rl.question("Enter your value\n",
+    (s)=> {
+    console.log(s);
+    rl.close();
+    process.exit();
+    }
+    );
+
